46567e3cefcada520e7c73845d4b5d402de01736
Nick Niles <nickyniles@gmail.com>
2018-01-16 19:28:51 +0100

    add gallery js

---
+++ _drafts/gallery.markdown
@@ -0,0 +1,6 @@
+---
+order: 8
+include: sections/normal.html
+title:  "Bilder"
+---
+{% include gallery.html %}
--- js/app.js
+++ js/app.js
@@ -130,3 +130,38 @@ function gameOver(score) {
     //    alert("Spende "+score+" Setzlinge.");
   }
 }
+
+
+// Gallery
+var openPhotoSwipe = function () {
+var pswpElement = document.querySelectorAll('.pswp')[0];
+console.log(pswpElement)
+
+// build items array
+var items = [
+  {
+    src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+    w: 964,
+    h: 1024
+  },
+  {
+    src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+    w: 1024,
+    h: 683
+  }
+];
+
+// define options (if needed)
+var options = {
+  // optionName: 'option value'
+  // for example:
+  index: 0, // start at first slide
+          history: false,
+  focus: false
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
+}
+document.getElementById('galleryBtn').onclick = openPhotoSwipe;
