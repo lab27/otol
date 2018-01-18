54c67c8202a075501520d504f6a362d651a08991
Nick Niles <nickyniles@gmail.com>
2018-01-18 10:19:15 +0100

    add gallery script

--- js/app.js
+++ js/app.js
@@ -132,36 +132,3 @@ function gameOver(score) {
 }
 
 
-// Gallery
-var openPhotoSwipe = function () {
-var pswpElement = document.querySelectorAll('.pswp')[0];
-console.log(pswpElement)
-
-// build items array
-var items = [
-  {
-    src: 'https://drive.google.com/open?id=1crbI4cE23k6hZ8w_Mxw1_UNsq_SI441V',
-    w: 964,
-    h: 1024
-  },
-  {
-    src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
-    w: 1024,
-    h: 683
-  }
-];
-
-// define options (if needed)
-var options = {
-  // optionName: 'option value'
-  // for example:
-  index: 0, // start at first slide
-          history: false,
-  focus: false
-};
-
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
-}
-document.getElementById('galleryBtn').onclick = openPhotoSwipe;
