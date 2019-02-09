ddeea06fb76ab316ce9d7956521a1e70d3aad3c5
phil <phil@branch14.org>
2019-02-09 22:00:51 +0100

    save saome bandwidth by using the gallery/full version instead of the upload version of images

--- process.sh
+++ process.sh
@@ -15,8 +15,12 @@ pwd
 
 ./process_all.sh
 
+# Save some bandwidth by using the gallery version of the images.
+sed -i 's|/uploads/|/gallery/full/|' ./_data/*.yml
+sed -i 's|/uploads/|/gallery/full/|' ./*/*.markdown
+
 git add .
-git commit -m "Automatic update via process_all.sh"
+git commit -m "Automatic update via `process.sh` :sunglasses:"
 git push || git pull && git push
 
 exit 0
